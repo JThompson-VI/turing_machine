39427603d9dde4cb1f6c3bda88669d48818061aa
Jacob Thompson <a01657923@usu.edu>
2023-11-22 15:46:26 -0700

    get tailwind running

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1 class="font-bold text-xl pb-2">Welcome to SvelteKit</h1>
+<p>Visit <a class="text-blue-400 underline" href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
