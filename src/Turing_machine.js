fe9c4b25d3d6603dae2b6c59f64241e228577a16
Jacob Thompson <a01657923@usu.edu>
2023-11-22 14:53:55 -0700

    MVP turing machine script

--- src/Turing_machine.js
+++ src/Turing_machine.js
@@ -1,92 +1,159 @@
-import assert from "assert";
 import { argv } from "process";
-import fs from 'fs';
+import fs from "fs";
 
 const program_config = {
-  name: 'Untitled program',
-  start_state: 'q0',
-  accept_state: 'qAccept'
-}
+  name: "Untitled program",
+  start_state: "q0",
+  accept_state: "qAccept", // TODO: accept mulitple accept states
+};
 
-const config_pattern = /^(name|start_state|accept_state):.*/
+const config_pattern = /^[\w]+:.*/;
+const comment_pattern = /^\/\//;
 
-const actions = {
-  '>': (head_pos, tape) => {
-    // move head right
-    if (head_pos === tape.length - 1) {
-      tape.push('_')
+function parse_file(raw_program) {
+  const program_lines = raw_program
+    .split("\n")
+    .map((line) => line.trim())
+    .filter((line) => line !== "" && !comment_pattern.test(line));
+
+  let config_done = false;
+  const instruction_lines = [];
+  for (let line of program_lines) {
+    if (config_pattern.test(line)) {
+      if (config_done) {
+        throw new Error(
+          "Program must be configured before the first instruction.",
+        );
+      }
+      const config_line = line.split(":");
+      switch (config_line[0]) {
+        case "name":
+          program_config.name = config_line[1].trim();
+          break;
+        case "start_state":
+          program_config.start_state = config_line[1].trim();
+          break;
+        case "accept_state":
+          program_config.accept_state = config_line[1].trim();
+          break;
+        default:
+          throw new Error(`'${config_line[0]}' is not a valid config option.`);
+      }
+    } else {
+      config_done = true;
+      instruction_lines.push(line);
     }
-    return head_pos + 1
-  },
-  '<': (head_pos, tape) => {
-    // move head left
-    if (head_pos === 0) {
-      tape.unshift('_')
-    }
-    return 0
-  },
-  '-': () => {
-    // hold/NOOP
-  },
-  'p': (head_pos, tape, symbol) => {
-    // print symbol at the current position
-    tape[head_pos] = symbol
-    return head_pos
   }
+  return instruction_lines;
 }
 
-function get_instructions() {
-  let input_file = "debug_inst.txt"
+function parse_instruction(instruction) {
+  const tokens = instruction.split(" ").filter((token) => token !== "");
+  if (tokens.length !== 4) {
+    throw Error(`Instruction: ${instruction} is not a valid instruction.`);
+  }
+  if (
+    tokens[1].length > 1 ||
+    (tokens[2].startsWith("p") && tokens[2].length > 2)
+  ) {
+    throw Error("sybmols must be one character in length.");
+  }
+  return {
+    state: tokens[0],
+    symbol: tokens[1],
+    action: tokens[2],
+    new_state: tokens[3],
+  };
+}
+
+function compile_instruction_graph(instruction_lines) {
+  const graph = {};
+  for (let instruction of instruction_lines) {
+    const { state, symbol, action, new_state } = parse_instruction(instruction);
+    if (!graph[state]) {
+      graph[state] = {};
+    }
+    graph[state][symbol] = {
+      action: action,
+      new_state: new_state,
+    };
+  }
+  return graph;
+}
+
+function report_compilation_error(raw_program, offending_instruction) {
+  // TODO: returns the error text and the line of the instruction in the original program.
+}
+
+function get_program() {
+  let input_file = "example_programs/is_divisible_by_three.txt";
   if (argv.length > 2) {
-    input_file = argv[2]
+    input_file = argv[2];
   }
-  let instructions = fs.readFileSync(input_file, 'utf8').split('\n')
-  instructions = instructions.map((inst) => inst.trim()).filter((inst) => inst !== '')
-  return instructions
+  let instructions = fs.readFileSync(input_file, "utf8");
+  return instructions;
 }
 
-function get_instruction_graph(instructions) {
-  const machine_graph = {}
-  for (let inst of instructions) {
-    const tokens = inst.split(" ").filter((x) => x != "")
-    if (tokens.length < 4) {
-      console.error(`The instruction ${inst} is not valid.`)
-    }
-    if (!machine_graph[tokens[0]]) {
-      machine_graph[tokens[0]] = {}
-    }
-    machine_graph[tokens[0]][tokens[1]] = {
-      action: tokens[2],
-      new_state: tokens[3]
-    }
+function get_tape() {
+  if (argv.length > 3) {
+    return argv[3].split("");
   }
-  return machine_graph
+  return "111111".split("");
 }
 
 function run_program(tape, instruction_graph, config) {
-  let state = config.start_state
-  let head_position = 0
-  let action_obj = instruction_graph[state]?.[tape[head_position]]
+  let { start_state } = config;
+  let head_position = 0;
+  let state = start_state;
+  let action_obj = instruction_graph[state]?.[tape[head_position]];
+  let timeout = 10000;
   while (action_obj) {
-    // TODO: impl run the program
-    if (action_obj.action === '<') {
-
-    } else if (action_obj.action === '>') {
-
-    } else if (action_obj.action.startsWith('p')) {
-
+    if (action_obj.action === "<") {
+      if (head_position === 0) {
+        tape.unshift("_");
+      } else {
+        head_position -= 1;
+      }
+      if (head_position < 0) {
+        throw Error("Read head fell off the infinite tape...");
+      }
+    } else if (action_obj.action === ">") {
+      if (head_position === tape.length - 1) {
+        tape.push("_");
+      }
+      head_position += 1;
+    } else if (action_obj.action.startsWith("p")) {
+      tape[head_position] = action_obj.action[1];
+    } else if (action_obj.action === "-") {
+      // NOOP
+    } else {
+      throw Error(`${action_obj.action} is an invalid action.`);
     }
-    break; // remove this
+    state = action_obj.new_state;
+    action_obj = instruction_graph[state]?.[tape[head_position]];
+    if (timeout < 1) {
+      throw Error("Hit timeout, likely a non halting program.");
+    }
+    timeout -= 1;
   }
-  return tape
+  return state;
 }
 
 function main() {
-  const instructions = get_instructions()
-  const machine_graph = get_instruction_graph(instructions)
-  const tape = ['1', '1', '0']
-  run_program(tape, machine_graph, program_config)
-  console.log(tape)
+  const program = get_program();
+  const tape = get_tape();
+  console.log("INPUT");
+  console.log(tape.join(""));
+
+  const machine_graph = compile_instruction_graph(parse_file(program));
+  const final_state = run_program(tape, machine_graph, program_config);
+  const input_accepted = final_state === program_config.accept_state;
+
+  console.log("OUTPUT");
+  console.log(tape.join(""));
+
+  console.log("FINAL STATE");
+  console.log("exit status: " + input_accepted);
 }
 
 main();
